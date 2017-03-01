9e0c14fed5d35a20ef2bfe154825308c984e5b7c
Tan Kah Hwa <kahhwa96@gmail.com>
2017-03-01 20:22:35 +0800

    change something

--- Web DIVES/php/loginorganizer.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-    <?php 
-        $email= $_POST["email"];
-        echo "$email";
-    ?>
-    <a href="../html/dashboard.html">Click Here to Continue</a>
-</body>
-</html>
--- WebDIVES/php/signuporganizer.php
+++ WebDIVES/php/signuporganizer.php
@@ -1,9 +1,14 @@
 <html>
 <body>
-    <?php 
+    <?php
         $email= $_POST["email"];
         echo "$email";
+        $password = $_POST["password"];
+        echo "$password";
+        $reenter = $_POST["reenter password"];
+        echo "$reenter";
     ?>
+
     <a href="../html/dashboard.html">Click Here to Continue</a>
 </body>
 </html>
