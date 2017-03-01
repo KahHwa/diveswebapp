0676514b091416197bfee4e70dd0c21d240e505f
Tan Kah Hwa <kahhwa96@gmail.com>
2017-03-01 21:31:05 +0800

    connection to azure database

--- WebDIVES/php/connection.php
+++ WebDIVES/php/connection.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$username = "b0ee69da112db5";
+$password = "55cc88e9";
+$database = "diveswebapp";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
