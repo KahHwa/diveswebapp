07f6367b03d57794f5e5a0c73a53a4598165650c
Tan Kah Hwa <kahhwa96@gmail.com>
2017-03-01 20:31:01 +0800

    f

--- WebDIVES/php/signuporganizer.php
+++ WebDIVES/php/signuporganizer.php
@@ -4,8 +4,7 @@
         $email= $_POST["email"];
         $password = $_POST["password"];
         $reenter = $_POST["reenter password"];
-        echo "$password";
-        echo <br>;
+        echo "$password<br>";
         echo "$reenter";
         echo "$email";
     ?>
