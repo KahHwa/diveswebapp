b505bda991cea9c2e06aaf9e09c2e45fb6286268
Tan Kah Hwa <kahhwa96@gmail.com>
2017-03-02 14:40:31 +0800

    f

--- WebDIVES/php/connection.php
+++ WebDIVES/php/connection.php
@@ -3,11 +3,12 @@ $servername = "ap-cdbr-azure-southeast-b.cloudapp.net";
 $username = "b0ee69da112db5";
 $password = "55cc88e9";
 $database = "diveswebapp";
-
+/*
 $servername1 = "localhost";
 $username1 = "root";
 $password1 = "";
 $database1 = "diveswebapp";
+*/
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
