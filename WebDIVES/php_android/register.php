d69a38bc9fcf14b349ec7eb217408ad548e3b35d
Rico Ardiya <ricoardiya@yahoo.com>
2017-03-15 22:25:25 +0800

    register.php

---
+++ WebDIVES/php_android/register.php
@@ -0,0 +1,18 @@
+<?php
+$con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");
+
+$firstname = $_POST["fname"];
+$lastname = $_POST["lname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$statement = mysqli_prepare($con, "INSERT INTO user (firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
+mysqli_stmt_bind_param($statement, "siss", $firstname, $lastname, $email, $password);
+mysqli_stmt_execute($statement);
+
+$response = array();
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
