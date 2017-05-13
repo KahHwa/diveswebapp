7e9f6c145ac5ff729f5064d52e0d32a51759495c
Rico Ardiya <ricoardiya@yahoo.com>
2017-05-13 16:40:58 +0800

    add more coloumn

--- WebDIVES/trial/integrated/select.php
+++ WebDIVES/trial/integrated/select.php
@@ -2,7 +2,7 @@
  //select.php  
  $connect = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");  
  $output = array();  
- $query = "SELECT Name, Skill1 FROM test_data";  
+ $query = "SELECT Name, Skill1, Skill2, Skill3 FROM test_data";  
  $result = mysqli_query($connect, $query);  
  if(mysqli_num_rows($result) > 0)  
  {  
