cd308d700771721f14422f58d16eed3c7d301bfb
Rico Ardiya <ricoardiya@yahoo.com>
2017-05-21 20:17:39 +0800

    add login android

---
+++ WebDIVES/php_android/login_testing.php
@@ -0,0 +1,22 @@
+ <?php  
+//  $db_name = "webappdb";  
+//  $mysql_user = "root";  
+//  $mysql_pass = "root";  
+//  $server_name = "localhost";  
+ $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp"); 
+
+ $user_name = $_POST["login_name"];  
+ $user_pass =  $_POST["login_pass"];  
+ $sql_query = "select Company_name from company_signup where Company_name like '$user_name' and Password like '$user_pass';";  
+ $result = mysqli_query($con,$sql_query);  
+ if(mysqli_num_rows($result) >0 )  
+ {  
+ $row = mysqli_fetch_assoc($result);  
+ $name =$row["Company_name"];  
+ echo "Login Success..Welcome ".$name;  
+ }  
+ else  
+ {   
+ echo "Login Failed.......Try Again..";  
+ }  
+ ?> 
