cb8754278eae867107860528f653a8db31e2e3d4
Rico Ardiya <ricoardiya@yahoo.com>
2017-06-08 17:27:55 +0700

    update login

--- WebDIVES/php/logincompany.php
+++ WebDIVES/php/logincompany.php
@@ -1,10 +1,37 @@
 <html>
 <body>
     <?php 
-        $email= $_POST["email"];
-        echo "$email";
+
+        require 'connection.php';
+
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $query = mysql_query("SELECT * FROM registercompany where EMAIL='$email'");
+        $result = mysql_fetch_array($query);
+        $row = mysql_num_rows($query);
+        if (($email == "") && ($password == ""))
+        {
+            echo "Login failed";
+            exit;
+        }
+        if($row != 0)
+        {
+            if($password != $result['PASS'])
+            {
+                echo "Wrong Password";
+            }
+            else
+            {
+                echo "Welcome to DIVES ". $result['COMPANY_NAME'];
+                echo "<br>";
+                echo "<a href="../html/dashboard.html">click here</a>";
+            }
+        }
+        else
+        {
+            echo "Cannot find company";
+        }
     ?>
-    <a href="../html/dashboard.html">Click Here to Continue</a>
 </body>
 </html>
 
