6b6818057092eb5a59bed2720c1b8ee3e3aa700f
Rico Ardiya <ricoardiya@yahoo.com>
2017-06-08 17:50:52 +0700

    update signjcmpany

--- WebDIVES/php/signupcompany.php
+++ WebDIVES/php/signupcompany.php
@@ -4,17 +4,25 @@
 
         require 'connection.php';
 
-        $companyName=$_POST["companyName"];
-        $email = $_POST["email"];
+        $email=$_POST["email"];
         $password = $_POST["password"];
+        $reenter = $_POST["reenterpassword"];
+        $query =mysql_query('SELECT * FROM registercompany where EMAIL='$email'');
+        $result=mysql_fetch_array($query);
+        $row = mysql_num_rows ($query);
+        
+        if ($row == 0){
+                $sql = "INSERT INTO signupcompany(email, password, reenterpassword) VALUES ('$email', '$password', '$reenter')";
 
-        $sql = "INSERT INTO registercompany(COMPANY_NAME, EMAIL, PASS) VALUES ('$companyName', '$email', '$password')";
-        if ($conn->query($sql) === TRUE) {
-         echo "New record created successfully ";}
-         else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+                if ($conn->query($sql) == TRUE) {
+                    echo "New record created successfully";}
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
         }
-
+        else{
+            echo "User Already Exist! " ;
+        }
     ?>
     <a href="../html/dashboard.html">Click Here to Continue</a>
 </body>
