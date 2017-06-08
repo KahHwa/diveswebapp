2a4804f5d1a8948937dc4f79fb0cfd580c7347ac
Rico Ardiya <ricoardiya@yahoo.com>
2017-06-08 18:11:00 +0700

    .

--- WebDIVES/php/signupcompany.php
+++ WebDIVES/php/signupcompany.php
@@ -4,15 +4,15 @@
 
         require 'connection.php';
 
+        $companyName=$_POST["companyName"];
         $email=$_POST["email"];
         $password = $_POST["password"];
-        $reenter = $_POST["reenterpassword"];
         // $query =mysqli_query('SELECT * FROM registercompany where EMAIL='$email'');
         // $result=mysqli_fetch_array($query);
         // $row = mysqli_num_rows($query);
         $row = 0;
         if ($row == 0){
-                $sql = "INSERT INTO signupcompany(email, password, reenterpassword) VALUES ('$email', '$password', '$reenter')";
+                $sql = "INSERT INTO registercompany(COMPANY_NAME, EMAIL, PASS) VALUES ('$email', '$email', '$password')";
 
                 if ($conn->query($sql) == TRUE) {
                     echo "New record created successfully";}
