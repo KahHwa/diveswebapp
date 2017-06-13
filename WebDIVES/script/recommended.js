c76e2cab62720af82b6d2884a88bc3013037f3c7
Rico Ardiya <ricoardiya@yahoo.com>
2017-06-13 18:14:44 +0700

    update rec

--- WebDIVES/script/recommended.js
+++ WebDIVES/script/recommended.js
@@ -1,5 +1,5 @@
 var app = angular.module("myapp",[]);  
- app.controller("jobcontroller", function($scope, $http){
+ app.controller("job_controller", function($scope, $http){
     $scope.displayData = function(){  
            $http.get("../php/select_recommended.php")  
            .success(function(data){  
