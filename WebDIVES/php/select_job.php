79bf69bad3856a30765b1af44c7712fd540a7c4b
Rico Ardiya <ricoardiya@yahoo.com>
2017-06-13 17:54:04 +0700

    update recommended

--- WebDIVES/php/select_job.php
+++ WebDIVES/php/select_job.php
@@ -2,7 +2,7 @@
  //select.php  
  $connect = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");  
  $output = array();  
- $query = "SELECT Id, Position, Requirement1, Requirement2, Requirement3, Vacancy, Applicants, Job_status FROM microsoft_job";  
+ $query = "SELECT Id, Position FROM microsoft_job";  
  $result = mysqli_query($connect, $query);  
  if(mysqli_num_rows($result) > 0)  
  {  
--- WebDIVES/script/recommended.js
+++ WebDIVES/script/recommended.js
@@ -1,7 +1,7 @@
 var app = angular.module("myapp",[]);  
  app.controller("jobcontroller", function($scope, $http){
     $scope.displayData = function(){  
-           $http.get("select.php")  
+           $http.get("select_job.php")  
            .success(function(data){  
                 $scope.names = data;  
            });  
