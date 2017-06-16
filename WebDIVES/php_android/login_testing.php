9d1e331b2867f2efbbbaa10c3d6264c4c81a0dc1
Rico Ardiya <ricoardiya@yahoo.com>
2017-06-16 16:57:11 +0700

    update login android

--- WebDIVES/php_android/login_testing.php
+++ WebDIVES/php_android/login_testing.php
@@ -7,13 +7,13 @@
 
  $user_name = $_POST["login_name"];  
  $user_pass =  $_POST["login_pass"];  
- $sql_query = "select USERNAME from company_signup where PASSWORD like '$user_pass';";  
+ $sql_query = "select USERNAME from user where PASSWORD like '$user_pass';";  
  $result = mysqli_query($con,$sql_query);  
  if(mysqli_num_rows($result) >0 )  
  {  
  $row = mysqli_fetch_assoc($result);  
- $name =$row["Company_name"];  
- echo "Login Success..Welcome ".$user_name;  
+ $name =$row["USERNAME"];  
+ echo "Login Success..Welcome ".$name;  
  }  
  else  
  {   
