78e90e5b4c673f42acf160fc6642f27c3d0b7907
Muhammad Rais Fawwazi <MUHAMMAD269@e.ntu.edu.sg>
2017-06-17 04:16:05 -0400

    update button

--- WebDIVES/script/upcoming.js
+++ WebDIVES/script/upcoming.js
@@ -1,5 +1,8 @@
 var app = angular.module("myapp", []);
 app.controller("eventcontroller", function($scope, $http){
+    $scope.registerEvent = function(){
+    	location.href = '/webdives/html/event.html';
+    }
     $scope.displayData = function(){
         $http.get("../php/select_event.php")
         .success(function(data){
