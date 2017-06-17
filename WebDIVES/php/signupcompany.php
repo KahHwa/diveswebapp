aea1f7c893f9fe30625be412d5032a6d5846eae3
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-17 10:49:11 +0700

    login

--- WebDIVES/php/signupcompany.php
+++ WebDIVES/php/signupcompany.php
@@ -2,12 +2,28 @@
 <body>
     <?php 
 
-        require 'connection.php';
+        //require 'connection.php';
+        $conn = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");
+        $check ="SELECT * FROM registercompany where EMAIL='$email', COMPANY_NAME ='$companyName' ";
+        $rs = mysqli_query($conn,$check);
+        $data=mysqli_fetch_array($rs,MYSQLI_NUM);
+        if ($data[0]>1){
+            echo "Username or Email Already Taken<br/>";
 
+        }
+        else{
+             $sql = "INSERT INTO registercompany(COMPANY_NAME, EMAIL, PASS) VALUES ('$companyName', '$email', '$password')";
+                if ($conn->query($sql) == TRUE) {
+                    echo "New record created successfully";}
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+        }
+        /*
         $companyName=$_POST["companyName"];
         $email=$_POST["email"];
         $password = $_POST["password"];
-        $query =mysqli_query("SELECT * FROM registercompany where EMAIL='$email'");
+        $query =mysqli_query("SELECT * FROM registercompany where EMAIL='$email', COMPANY_NAME ='$companyName' ");
         $result=mysqli_fetch_array($query);
         $row = mysqli_num_rows($query);
         // $row = 0;
@@ -16,13 +32,12 @@
         }
         else{
               $sql = "INSERT INTO registercompany(COMPANY_NAME, EMAIL, PASS) VALUES ('$companyName', '$email', '$password')";
-
                 if ($conn->query($sql) == TRUE) {
                     echo "New record created successfully";}
                 else {
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
-        }
+        }*/
     ?>
     <a href="../html/dashboard.html">Click Here to Continue</a>
 </body>
