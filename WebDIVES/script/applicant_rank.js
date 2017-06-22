ed45bbc1d81163dd26f90a79cb7f8cd1755687da
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-22 23:24:43 +0700

    ..

--- WebDIVES/script/applicant_rank.js
+++ WebDIVES/script/applicant_rank.js
@@ -1,7 +1,6 @@
 var app = angular.module("appl",[]);  
  app.controller("rank_controller", function($scope, $http){
-
-      $scope.dis = function(){  
+      $scope.action = function(){  
            $http.get("../php/display_applicant.php")  
            .success(function(data){  
                 $scope.or()= data;  
