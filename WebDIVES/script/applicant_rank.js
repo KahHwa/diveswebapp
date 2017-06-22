ed6659c0ec26da49915b7c86048e5a7c192b7255
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-23 01:15:49 +0700

    new

--- WebDIVES/script/applicant_rank.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("appl",[]);  
- app.controller("rank_controller", function($scope, $http){
-      $scope.action = function(){  
-           $http.get("../php/display_applicant.php")  
-           .success(function(data){  
-                $scope.or()= data;  
-           });  
-      }
- });  
--- WebDIVES/script/recommended.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("app",[]);  
- app.controller("job_controller", function($scope, $http){
-      $scope.display = function(){  
-           $http.get("../php/select_recommended.php")  
-           .success(function(data){  
-                $scope.order = data;  
-           });  
-      }
-});  
