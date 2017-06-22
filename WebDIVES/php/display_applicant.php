5b07877e2d2cb3dfd59d3d9a93187a175446047a
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-22 17:06:03 +0700

    ..

--- WebDIVES/php/display_applicant.php
+++ WebDIVES/php/display_applicant.php
@@ -2,19 +2,21 @@
  //select.php  
  $connect = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");  
  $data = json_decode (file_get_contents("php://input"));
-$req1 = $data->req1;
-$req2 = $data->req2;
-$req3 = $data->req3;
-  
- $output = array();
- $query = "SELECT Id, Name, Skill1, Skill2, Skill3 FROM test_data WHERE Skill1='req1' or Skill2='req2' or Skill3='req3'  ";  
- $result = mysqli_query($connect, $query); 
- if(mysqli_num_rows($result) > 0)  
- {  
-      while($row = mysqli_fetch_array($result))  
-      {  
-           $output[] = $row;  
-      }  
-      echo json_encode($output);  
- }  
+ if (count($data)>0){
+    $req1 = $data->req1;
+    $req2 = $data->req2;
+    $req3 = $data->req3;
+    
+    $output = array();
+    $query = "SELECT Id, Name, Skill1, Skill2, Skill3 FROM test_data WHERE Skill1='$req1' or Skill2='$req2' or Skill3='$req3'  ";  
+    $result = mysqli_query($connect, $query); 
+    if(mysqli_num_rows($result) > 0)  
+    {  
+        while($row = mysqli_fetch_array($result))  
+        {  
+            $output[] = $row;  
+        }  
+        echo json_encode($output);  
+    }  
+ }
  ?>
