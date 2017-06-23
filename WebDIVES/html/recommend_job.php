10b44bd171bd78be4d46007369afbdffb7ed9d56
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-23 15:10:38 +0700

    ..

--- WebDIVES/html/recommend_job.php
+++ WebDIVES/html/recommend_job.php
@@ -2,7 +2,11 @@
 if (!isset ($_GET['JobId'])){
      include ("recommend_home.php");
  }
-    $sql = "SELECT test_data.Id AS USERID, test_data.Name AS USERNAME, test_data.Skill1 AS SKILL1, test_data.Skill2 AS SKILL2, test_data.Skill3 AS SKILL3, microsoft_job.Id AS JOBID, microsoft_job.Position AS JOBTITLE FROM test_data JOIN microsoft_job ON test_data.Skill1=microsoft_job.Requirement1 or test_data.Skill2=microsoft_job.Requirement2 or test_data.Skill3=microsoft_job.Requirement3 WHERE microsoft_job.Id=".$_GET['JobId']; 
+    $sql = "SELECT test_data.Id AS USERID, test_data.Name AS USERNAME, test_data.Skill1 AS SKILL1, test_data.Skill2 AS SKILL2, test_data.Skill3 AS SKILL3, microsoft_job.Id AS JOBID, microsoft_job.Position AS JOBTITLE FROM test_data JOIN microsoft_job 
+    ON test_data.Skill1=microsoft_job.Requirement1 or test_data.Skill1=microsoft_job.Requirement2 or test_data.Skill1=microsoft_job.Requirement3 
+    or test_data.Skill2=microsoft_job.Requirement1 or test_data.Skill2=microsoft_job.Requirement2 or test_data.Skill2=microsoft_job.Requirement3
+    or test_data.Skill3=microsoft_job.Requirement1 or test_data.Skill3=microsoft_job.Requirement2 or test_data.Skill3=microsoft_job.Requirement3 
+    WHERE microsoft_job.Id=".$_GET['JobId']; 
     $sql1= "SELECT * FROM microsoft_job WHERE microsoft_job.Id=".$_GET['JobId'];                                
     $connect = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");  
     
@@ -35,6 +39,7 @@ if (!isset ($_GET['JobId'])){
                    do {
                    ?>
                 <tr>
+                    
                   <td><?php echo $result['USERID']; ?></td>  
                   <td><?php echo $result['USERNAME']; ?></td>  
                   <td><?php echo $result['SKILL1']; ?></td>
