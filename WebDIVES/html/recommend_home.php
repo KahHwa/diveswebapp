89324545dde65a6c8e81895e1036fb0266f9c102
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-23 14:58:11 +0700

    ..

--- WebDIVES/html/recommend_home.php
+++ WebDIVES/html/recommend_home.php
@@ -1,6 +1,6 @@
 <table class="table table-bordered">  
                                 <tr>  
-                                    <th>JobId </th>
+                                    <th>Job Id </th>
                                     <th>Job Name</th>  
                                     <th>Requirement 1</th>
                                     <th>Requirement 2</th>
@@ -10,18 +10,18 @@
                     
                                     <?php
                                       $connect = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "b0ee69da112db5", "55cc88e9", "diveswebapp");  
-                                      $sql= "SELECT * FROM microsoft_job";
-                                      $query = mysqli_query($connect, $sql);
-                                      $result = mysqli_fetch_assoc($query);      
+                                      $sq= "SELECT * FROM microsoft_job";
+                                      $q = mysqli_query($connect, $sq);
+                                      $res = mysqli_fetch_assoc($q);      
                                     do {
                                     ?>
                                 <tr>
-                                        <td><?php echo $result['Id']; ?></td>  
-                                        <td><?php echo $result['Position']; ?></td>  
-                                        <td><?php echo $result['Requirement1']; ?></td>
-                                        <td><?php echo $result['Requirement2']; ?></td>
-                                        <td><?php echo $result['Requirement3']; ?></td>
-                                        <td><a class="btn btn-info btn-xs" href="recommend.php?page=job&JobId=<?php echo $result['Id'];?>">Show Rank For <?php echo $result['Position']; ?></a></td>
+                                        <td><?php echo $res['Id']; ?></td>  
+                                        <td><?php echo $res['Position']; ?></td>  
+                                        <td><?php echo $res['Requirement1']; ?></td>
+                                        <td><?php echo $res['Requirement2']; ?></td>
+                                        <td><?php echo $res['Requirement3']; ?></td>
+                                        <td><a class="btn btn-info btn-xs" href="recommend.php?page=job&JobId=<?php echo $res['Id'];?>">Show Rank For <?php echo $res['Position']; ?></a></td>
                                 </tr>  
-                                <?php }while($result=mysqli_fetch_assoc($query))?>
+                                <?php }while($res=mysqli_fetch_assoc($q))?>
 </table>
