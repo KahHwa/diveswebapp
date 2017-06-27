3a52d0b96e04336303f98b3afd0a29b1d6c86c8b
Henrikus <HENR0011@e.ntu.edu.sg>
2017-06-27 21:40:39 +0700

    ..

--- WebDIVES/html/recommend_job.php
+++ WebDIVES/html/recommend_job.php
@@ -74,7 +74,7 @@ $sql1= "SELECT * FROM microsoft_job WHERE microsoft_job.Id=".$_GET['JobId'];
                    do {
                    ?>
                 <tr>
-                  <td> <?php echo $idx; ?> </td>
+                  <td><?php echo $idx; ?> </td>
                   <td><?php echo $results['Id']; ?></td>  
                   <td><?php echo $results['Name']; ?></td>  
                   <td><?php echo $results['Skill1']; ?></td>
@@ -82,7 +82,7 @@ $sql1= "SELECT * FROM microsoft_job WHERE microsoft_job.Id=".$_GET['JobId'];
                   <td><?php echo $results['Skill3']; ?></td>
                   <td><?php echo $results['Percentage']; ?></td>
                 </tr>  
-            <?php $idx +=1;}while(mysqli_fetch_assoc( $results=mysqli_fetch_assoc($querys)))?>
+            <?php $idx +=1;}while($results=mysqli_fetch_assoc($querys))?>
            </table>
         </div>
         
